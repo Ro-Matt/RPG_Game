26706d66cc5296a9aa8e41d5db0786288a53a574
Matthew <mroja001@odu.edu>
2022-07-20 12:08:05 -0500

    added health bars and text -MR

--- Final_kono.py
+++ Final_kono.py
@@ -13,6 +13,12 @@ screen_height = 400 + bottom_panel
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('FIGHT TO YOUR HEARTS CONTENT')
 
+#define FONTS
+font = pygame.font.SysFont('Runic MT Condensed' , 26)
+
+#define COLORS
+red = (255, 0, 0)
+green = (0, 255, 0)
 
 #load images
 #background
@@ -22,14 +28,25 @@ background_img = pygame.image.load('img/Background/background.png').convert_alph
 panel_img = pygame.image.load('img/Icons/panel.png').convert_alpha()
 
 
+
+#create function for TEXT
+def draw_text(text, font, text_color, x, y):
+    img = font.render(text, True, text_color)
+    screen.blit(img, (x, y))
+
+
 #draw background
 def draw_bg():
     screen.blit(background_img, (0,0))
 
 def draw_panel():
+    #draw panel rectangle
     screen.blit(panel_img, (0,screen_height - bottom_panel))
-
-
+    #show knight stats
+    draw_text(f'{knight.name} HP: {knight.hp}', font, red, 100, screen_height - bottom_panel + 10)
+    for count, i in enumerate(bandit_list):
+        #show name and health
+        draw_text(f'{i.name} HP: {i.hp}', font, red, 550, (screen_height - bottom_panel + 10) + count * 60)
 #fighter
 class Fighter():
     def __init__(self, x, y, name, max_hp, strength, potions):
@@ -79,10 +96,25 @@ class Fighter():
         #loop animation at the end of index
         if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index = 0
-        
-        
+                
     def draw(self):
         screen.blit(self.image, self.rect)
+
+class HealthBar():
+    def __init__(self, x, y ,hp, max_hp):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.max_hp = max_hp
+        
+    def draw(self, hp):
+        #update with new hp
+        self.hp = hp
+        #calculate health ratio
+        ratio = self.hp / self.max_hp
+        pygame.draw.rect(screen, red, (self.x, self.y, 150, 20))
+        pygame.draw.rect(screen, green, (self.x, self.y, 150 * ratio, 20))
+
         
 knight = Fighter(200, 260, 'Knight', 30, 10, 3)
 bandit1 = Fighter(550, 270, 'Bandit', 20, 6, 1)
@@ -92,6 +124,12 @@ bandit_list = []
 bandit_list.append(bandit1)
 bandit_list.append(bandit2)
 
+#HEALTH BARS
+knight_health_bar = HealthBar(100, screen_height - bottom_panel + 40, knight.hp, knight.max_hp)
+bandit1_health_bar = HealthBar(550, screen_height - bottom_panel + 40, bandit1.hp, bandit1.max_hp)
+bandit2_health_bar = HealthBar(550, screen_height - bottom_panel + 100, bandit2.hp, bandit2.max_hp)
+
+
 run = True
 while run:
     
@@ -99,7 +137,12 @@ while run:
     
     #draw background
     draw_bg()
+    
+    #draw panel
     draw_panel()
+    knight_health_bar.draw(knight.hp)
+    bandit1_health_bar.draw(bandit1.hp)
+    bandit2_health_bar.draw(bandit2.hp)
     
     #draw fighters
     knight.update()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RPG_Game
 Hello this is my first attempt at creating a turn based RPG like the early Final Fantasy games.
+Using youtube: Coding with Russ tutorial
